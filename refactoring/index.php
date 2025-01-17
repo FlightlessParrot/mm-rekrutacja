7e839ddacb7e5e96f4020ce0cde56d29e4e13606
Konrad <shrimpinweb@gmail.com>
2025-01-17 22:21:32 +0100

    And one more commit

--- refactoring/index.php
+++ refactoring/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'support/MysqlManager.php';
-require_once 'support/ActionExecuter.php';
+require_once __DIR__ . '/support/MysqlManager.php';
+require_once __DIR__ . '/support/ActionExecuter.php';
 $pdo = new PDO('mysql:host=localhost;dbname=refactoring', 'root', 'root');
 $mysqlManager = new MysqlManager($pdo);
 $actionExecuer = new ActionExecuter($mysqlManager, $_GET);
